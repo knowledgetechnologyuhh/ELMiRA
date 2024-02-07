298d73567a08463e5837d188e708351bb4d52f04
oo222bs <ozan_bjk93@hotmail.com>
2024-02-07 12:28:18 +0100

    Image2RealWorld model has been trained

--- src/image2real_trainer.py
+++ src/image2real_trainer.py
@@ -1,6 +1,5 @@
 import torch
-from src.image2real import ImageToRealWorldMLP
-import os
+from src.image2real import ImageToRealWorldMLP, Image2RealDataset
 import numpy as np
 
 from torch.utils.data import DataLoader
@@ -10,14 +9,16 @@ import datetime
 import wandb
 
 def loss(output, ground_truth):
-    loss = torch.mean(torch.square(output - ground_truth))
+    loss = torch.nn.functional.smooth_l1_loss(output, ground_truth, beta=0.01)#torch.mean(torch.square(output - ground_truth))
     return loss
 
 def validate(model, batch, epoch_loss_t):
-    output = model(batch['input'])
-    mse_loss = loss(output, batch['ground_truth'])  # compute loss
-    epoch_loss_t.append(mse_loss.item())    # record the batch loss
-    return mse_loss # return the loss
+    with torch.no_grad():
+        output = model(batch['input'])
+        mse_loss = loss(output, batch['ground_truth'])  # compute loss
+        euclidean_distance = torch.linalg.norm(output-batch['ground_truth'], dim=1).mean()
+        epoch_loss_t.append(mse_loss.item())    # record the batch loss
+    return mse_loss, euclidean_distance # return the loss
 
 def train(model, batch, optimiser, epoch_loss):
     output = model(batch['input'])
@@ -28,10 +29,11 @@ def train(model, batch, optimiser, epoch_loss):
     return mse_loss # return the loss
 
 def main():
-    num_of_epochs=100
-    log_interval=10
-    test=True
-    test_interval=1
+    num_of_epochs = 50000
+    log_interval = 100
+    test = True
+    test_interval = 1
+    batch_size=5
     # 1. Start a new run for WandB
     run = wandb.init(project="train-image2real")
 
@@ -57,72 +59,45 @@ def main():
     # 3. Log gradients and model parameters
     run.watch(model)
     # Initialise the optimiser
-    optimiser = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optimiser = torch.optim.Adam(model.parameters(), lr=1e-5)
 
     scheduler = MultiStepLR(optimiser, milestones=[10000], gamma=0.5)
-    #scheduler = ReduceLROnPlateau(optimiser, mode='min', factor=0.5, patience=3)
-    #  Inspect the model with tensorboard
-    model_name = 'xbit_res34_mlpactiondec_three_buttons_120_vars_all_signals_025do'
-    date = str(datetime.datetime.now()).split('.')[0]
-    #writer = SummaryWriter(log_dir='.././logs/'+model_name+date)  # initialize the writer with folder "./logs"
-
-    # Load the trained model
-    #checkpoint = torch.load(save_dir + '/xbit_res34_mlpactiondec_tap_fixed_execonly_pose__500epochs.tar')       # get the checkpoint
-    #model.load_state_dict(checkpoint['model_state_dict'])       # load the model state
-    #optimiser.load_state_dict(checkpoint['optimiser_state_dict'])   # load the optimiser state
 
     model.train()  # tell the model that it's training time
     # Load the dataset
-    input_coordinates = open('../data/input_coordinates.txt', 'r')
-    inputs = input_coordinates.read().splitlines()
-    output_coordinates = open('../data/output_coordinates.txt', 'r')
-    outputs = output_coordinates.read().splitlines()
-    training_data_input = []
-    training_data_output = []
-    for i, inp in enumerate(inputs):
-        row = inp.split('\t')
-        row_out = outputs[i].split('\t')
-        for j, cell in enumerate(row):
-            training_data_input.append(cell.split('-'))
-            training_data_output.append(row_out[j].split('-'))
-    training_data = {'input':training_data_input, 'ground_truth':training_data_output}
-    train_dataloader = DataLoader(training_data, batch_size=5, shuffle=True)
-    # Load the dataset
-    input_coordinates_val = open('../data/input_coordinates_val.txt', 'r')
-    inputs_val = input_coordinates_val.read().splitlines()
-    output_coordinates_val = open('../data/output_coordinates_val.txt', 'r')
-    outputs_val = output_coordinates_val.read().splitlines()
-    val_data_input = []
-    val_data_output = []
-    for i, inp in enumerate(inputs_val):
-        row = inp.split('\t')
-        row_out = outputs_val[i].split('\t')
-        for j, cell in enumerate(row):
-            val_data_input.append(cell.split('-'))
-            val_data_output.append(row_out[j].split('-'))
-    val_data = {'input':val_data_input, 'ground_truth':val_data_output}
-    val_dataloader = DataLoader(val_data, batch_size=5, shuffle=True)
+    training_data = Image2RealDataset('../data', 'train')
+    val_data = Image2RealDataset('../data', 'val')
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
     step = 0
     for epoch in range(num_of_epochs):
         epoch_loss = []
         for input in train_dataloader:
+            input['input'] = input['input'].to(device)
+            input['ground_truth'] = input['ground_truth'].to(device)
             loss = train(model, input, optimiser, epoch_loss)
             print("step:{} loss:{}".format(step, loss))
+            step = step+1
         #4. Log the losses
-        run.log({"Training Loss": np.mean(epoch_loss)})
+        run.log({"Training Loss": np.mean(epoch_loss)}, epoch)
         scheduler.step()
         # Validation
         if test and (epoch + 1) % test_interval == 0:
             epoch_loss_t = []
             for input in val_dataloader:
-                loss = validate(model, input, epoch_loss_t)
+                input['input'] = input['input'].to(device)
+                input['ground_truth'] = input['ground_truth'].to(device)
+                loss, euclidean_distance = validate(model, input, epoch_loss_t)
+                print("validation")
                 print("step:{} loss:{}".format(step, loss))
-            run.log({"Validation Loss": np.mean(epoch_loss_t)})
+            run.log({"Validation Loss": np.mean(epoch_loss_t)}, epoch)
+            run.log({"Validation Mean Euclidean Distance": euclidean_distance}, epoch)
         # Save the model parameters at every log interval
         if (epoch+1) % log_interval == 0:
             torch.save({'model_state_dict': model.state_dict(),
                         'optimiser_state_dict': optimiser.state_dict()},
-                       '../image2real_model_'+str(epoch+1)+'epochs.tar')
+                       '../model_checkpoints/image2real_model_'+str(epoch+1)+'epochs.tar')
 
     wandb.finish()
 if __name__ == '__main__':
